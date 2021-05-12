f46b94f807421f11279b6b0e82e7214ae9228355
enrique mejia <emejiaflorez@gmail.com>
2021-05-12 14:17:57 -0500

    Ejercicios 06-JS-V

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -7,13 +7,47 @@ function crearUsuario() {
   // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
-  // Tu código:
-}
+  
+  // 1. Tu código:
+    
+  class Usuario {
+        constructor(opciones){
+          this.usuario  = opciones.usuario;
+          this.nombre   = opciones.nombre;
+          this.email    = opciones.email;
+          this.password = opciones.password;
+      }
+      saludar() {
+         return ('Hola, mi nombre es ' + this.nombre);
+      }
+   }
+  return Usuario;
+
+  //   SOLUCION
+  
+  //  function Usuario(opciones) {
+  //   this.usuario = opciones.usuario;
+  //   this.nombre = opciones.nombre;
+  //   this.email = opciones.email;
+  //   this.password = opciones.password;
+  // }
+
+  // Usuario.prototype.saludar = function() {
+  //   return 'Hola, mi nombre es ' + this.nombre;
+  // };
+
+  // return Usuario;
+
+  }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
-  // Tu código:
+  // 2. Tu código:
+
+  Constructor.prototype.saludar=function(){
+    return 'Hello World!';
+  }
 }
 
 function agregarStringInvertida() {
@@ -22,7 +56,19 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+ 
+  // 3. Tu código:
+ 
+  String.prototype.reverse = function() {
+    var cadenaInversa = '';
+    
+    for (var i = this.length - 1; i >= 0; i--) {
+      cadenaInversa = cadenaInversa + this[i];
+    }
+    return cadenaInversa;
+  };
 }
+
 
 // ---------------------------------------------------------------------------//
   //Crea el constructor de la clase "Persona"
@@ -34,26 +80,66 @@ function agregarStringInvertida() {
     //   Edad: 22,
     //   Domicilio: 'Saavedra 123'
     //  }
-
-  class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    
+    /*Escribir los argumentos que recibe el constructor*/ 
       // Crea el constructor:
 
+  class Persona {
+    constructor (nombre,apellido,edad,domicilio) {
+      this.nombre   = nombre;
+      this.apellido = apellido;
+      this.edad     = edad;
+      this.domicilio= domicilio;
     }
-}
-
+     detalle(){
+        return {
+         nombre    : this.nombre,
+         apellido  : this.apellido,
+         edad      : this.edad,
+         domicilio : this.domicilio
+       }
+     } 
+  }
+    
+  // class Persona {
+  //   constructor(nombre, apellido, edad, domicilio) {
+  //     // Tu código:
+  //     this.nombre = nombre,
+  //     this.apellido = apellido,
+  //     this.edad = edad,
+  //     this.domicilio = domicilio
+  //     this.detalle = function() {
+  //       return { 
+  //         Nombre: this.nombre,
+  //         Apellido: this.apellido,
+  //         Edad: this.edad,
+  //         Domicilio: this.domicilio
+  //       }
+  //     }
+  //   }
+  // }
+ 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  
+  // 4. Tu codigo
+  var persona = new Persona(nombre,apellido,edad,dir);
+  return persona;
+
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
-}
+ 
+  // 5. Tu codigo
+  Persona.prototype.datos = function(){
+    return this.nombre + ", " + this.edad + " años";
+  }
   
-
+}
 // No modificar nada debajo de esta línea
 // --------------------------------
 
